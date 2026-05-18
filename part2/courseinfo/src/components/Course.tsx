37189d0f2eabf07d85cb1cf74b6391e58f8ca41c
Fabian Brandes <fabian.brandes@cgi.com>
2026-05-18 16:41:32 +0200

    exercise 2.2

--- part2/courseinfo/src/components/Course.tsx
+++ part2/courseinfo/src/components/Course.tsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Content from "./Content";
+import Total from "./Total";
 
 const Course = ({ course }) => (
     <>
         <Header course={course} />
         <Content parts={course.parts} />
-
+        <Total parts={course.parts} />
     </>
 );
 
--- part2/courseinfo/src/components/Total.tsx
+++ part2/courseinfo/src/components/Total.tsx
@@ -1,3 +1,9 @@
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+const Total = ({ parts }) => {
+    let sum = 0;
+    for (let i = 0; i < parts.length; i++) {
+        sum += parts[i].exercises;
+    }
+    return <b>Total of {sum} exercises</b>;
+};
 
 export default Total
