309d63ac4a1e3c66fe7cae6463428c8c2e65501f
Fabian Brandes <fabian.brandes@cgi.com>
2026-05-27 16:15:45 +0200

    exercise 7.2 and 7.3

--- part7/routed-anecdotes/src/components/CreateNew.jsx
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -3,15 +3,21 @@ import { useNavigate } from 'react-router-dom'
 import {useField} from "../hooks/useField";
 
 const CreateNew = ({ addAnecdote }) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
+  const {reset: resetContent, ...content} = useField('text')
+  const {reset: resetAuthor, ...author} = useField('text')
+  const {reset: resetInfo, ...info} = useField('text')
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     addAnecdote({ content, author, info, votes: 0 })
     navigate('/')
+  }
+
+  const resetForm = (e) => {
+    resetAuthor()
+    resetContent()
+    resetInfo()
   }
 
   return (
@@ -31,6 +37,7 @@ const CreateNew = ({ addAnecdote }) => {
           <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
         </div>
         <button>create</button>
+        <button type='submit' onClick={resetForm}>reset</button>
       </form>
     </div>
   )
